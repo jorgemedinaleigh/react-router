efc6ba97fd5173c23ef25a81ccd63d498c272c75
jorgemedinaleigh <jorgemedinaleigh@protonmail.com>
2025-07-20 17:20:08 -0400

    added content for the routes

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -2,6 +2,31 @@ import React from 'react'
 
 export const Contact = () => {
   return (
-    <div>Contact</div>
+    <>
+      <div className="spinner-border text-primary" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+      <div className="spinner-border text-secondary" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+      <div className="spinner-border text-success" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+      <div className="spinner-border text-danger" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+      <div className="spinner-border text-warning" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+      <div className="spinner-border text-info" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+      <div className="spinner-border text-light" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+      <div className="spinner-border text-dark" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    </>
   )
 }
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,6 +2,37 @@ import React from 'react'
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <table class="table table-dark table-striped-columns">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>John</td>
+            <td>Doe</td>
+            <td>@social</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
   )
 }
